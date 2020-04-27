5912a5ac4cf326a706e0fa3115ac5d3b9175f564
Ashutosh Kumar <58232141+ashu1069@users.noreply.github.com>
2020-04-27 16:39:52 +0530

    Add files via upload

---
+++ Modular division.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 16:26:08 2020
+
+@author: user
+"""
+
+#Modular division
+def extended_gcd(a,n):
+    assert a>=0 and n>=0 and a+n>0
+    if n==0:
+        t,s=0,1
+    else:
+        p,q=extended_gcd(n,a%n)
+        s=q
+        t=p-q*(a//n)
+    assert 1==a*s+n*t
+    return (s,t)
+print(extended_gcd(3,5))
