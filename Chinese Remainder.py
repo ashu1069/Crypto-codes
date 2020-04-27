97d2727e37054dcb8e2ba3bd40e8b94c89dbe245
Ashutosh Kumar <58232141+ashu1069@users.noreply.github.com>
2020-04-27 17:01:12 +0530

    Add files via upload

---
+++ Chinese Remainder.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 27 16:46:46 2020
+
+@author: user
+"""
+
+#Chinese Remainder theorem
+#a,b are coprimes
+def extended_gcd(a,b):
+    assert a>=0 and b>=0 and a+b>0
+    if b==0:
+        x,y=1,0
+    else:
+        p,q=extended_gcd(b,a%b)
+        x=q
+        y=p-q*(a//b)
+    assert 1==a*x+b*y
+    return (x,y)
+def ChineseRemainder(a,ra,b,rb):
+    x,y=extended_gcd(a,b)
+    r=ra*b*y+rb*a*x
+    m=a*b
+    return ((r%m+m)%m)
+print(ChineseRemainder(3,2,5,1))
